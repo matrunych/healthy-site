a772accad140b557c80ab199439d4ff972918a38
matrunych <47101257+matrunych@users.noreply.github.com>
2020-05-11 03:01:59 +0300

    Add files via upload

---
+++ src/components/articles/article2.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import Foot from '../footer';
+
+import './article.css'
+
+export default class Article2 extends React.Component {
+    render(){
+        return(
+            <div>
+      <div className="article">
+      
+      <h1>7 Healthy Habits for a Healthy Life</h1> 
+    
+           
+           <article id="post-19484" class="post-19484 post type-post status-publish format-
+           standard has-post-thumbnail category-featured category-health-fitness">
+               <div class="entry-content">
+                  
+                       <img class="center-image" src="https://d1v1n2d34vm51j.cloudfront.net/wp-content/uploads/Well-7-Healthy-Habits-4p-3p-2p-B_web1.jpg"
+                        ></img>
+                           <p>It’s a truism that most of us want to live long, happy, successful, and healthy lives. Unfortunatel
+                               y, in our pursuit of success we often take shortcuts with our health—and wind up suffering from vari
+                               ous ailments and disabilities that we could have avoided.</p>
+                            <p >It doesn’t have to be that way at all. Though many of us live stressful, demanding lives, with
+                                 just a little tweaking here and there, we can develop habits that will help us live healthier and more productive lives.</p>
+                                 <p>There is no shortage of information available on suggested tips for living a healthy lifestyle—one book we saw suggested no less 
+                                     than 107 healthy habits! We won’t get that exhaustive, but we pinpointed the most prevalent seven healthy habits that anyone shoul
+                                     d be able to include in their daily lives.</p>
+                                     <h2>1. Get your exercise</h2>
+                                     <p >Regular exercise is probably the closest we can get to a fountain of youth. According to the National Cancer Institute, regular
+                                          exercise helps control weight, maintain healthy bones, muscles, and joints, and reduces our risk of high blood pressure, heart 
+                                          disease, and diabetes. Further, about 260,000 deaths per year in the U.S. are attributable to the lack of physical activity.</p>
+                                    <p >Many exercise authorities suggest 30 minutes of exercise, 5-6 days a week, giving your body one day to rest and recuperate. The exercise doesn’t have to be
+                                         a gut-wrenching, iron-man type experience. Something as simple as a brisk 30-minute walk can work wonders for your health and 
+                                         literally add years to your life. And it can be supplemented by taking the stairs at work, a 10-15 minute walk during lunch, 
+                                         or having a small pedaling device at your desk. The main thing is to find exercise that you enjoy, not something that’s an ordeal.</p>
+                                         
+                                    <h2>2. Always eat breakfast</h2>
+                                    <p >Research shows that people who have breakfast tend to take in more vitamins and minerals and less fat and cholesterol.
+                                         Eating things that are high in fiber and proteins keeps you feeling full and energized. These include whole-grain cereals and 
+                                         breads, low-fat milk, fruit, and yogurt.</p>
+                                         <p >
+                                             <img class="center-image" src="https://d1v1n2d34vm51j.cloudfront.net/wp-content/uploads/Well-7-Healthy-Habits-4p-3p-2p-B_web4.jpg" ></img>
+                                            <h2>3. Practice healthy eating throughout the day</h2>
+                                            <p> This habit includes such things as eating more fruit and nuts and avoiding sugary drinks and snacks. At meal time, the Ameri
+                                            can Heart Association recommends a serving of fish twice a week. Besides being a rich source of protein, fatty fish (mackerel, 
+                                            salmon, lake trout, herring, sardines, and albacore tuna) have omega-3 fatty acids which reduce the threat of heart disease.</p>
+                                            <p >Don’t forget portion control. If you want to live to be 100, go for larger portions of fruits and vegetables rich in vitamins, minerals, and fiber, and consume smaller portions 
+                                                of higher calorie foods containing large amounts of sugar and fats.</p>
+                                                <p >And chew your food! Many nutritionists recommend chewing each mouthful 20-30 times to get it into its most
+                                                digestible form. Studies have also shown that chewing slowly reduces calorie intake by about 10%, partly because it takes your stomach 
+                                                about 20 minutes to tell the brain that it’s full.</p>
+                                                <p>Finally, one other cautionary note regarding a healthy eating habit: be wary of artificial sweeteners. A study conducted over a 10-year period
+                                                 at the University of Manitoba and published in the Canadian Medical Association Journal found that artificial sweeteners may be 
+                                                 associated with an increased risk of obesity, long-term weight gain, diabetes, high blood pressure and heart disease. Dr. Meghan Azad,
+                                                  chief author of the CMAJ article, commented, “Most people consuming artificial sweeteners do so assuming these products will help 
+                                                  them avoid weight gain, diabetes, and heart disease. Yet we are seeing the opposite association from multiple studies.”</p>
+                                            <h2>4. Stay hydrated</h2>
+                                            <p>Getting the proper amount of water is extremely important as every cell, tissue, and organ in our bodies needs water.
+                                                 Traditionally we’re told we need eight 8-ounce glasses of water daily, an amount that’s never been substantiated medically. 
+                                                 Perhaps a better guide is to try to drink enough water that you urinate once every 2-4 hours, and the urine is light in color.</p>
+                                                 <p>To help develop and keep this habit, many devices, from “smart bottles” to numerous free apps, are readily accessible to keep you properly hydrated.</p>
+                                            <h2>5. Don’t neglect dental hygiene</h2>
+                                            <p >At the end of a long day, how many take the time to floss? Some studies indicate that regular flossing could add over 6 years to your life. Why? The 
+                                                theory is that the bacteria that produce dental plaque enter the bloodstream and are somehow associated with inflammation that blocks blood vessels and
+                                                 causes heart disease. So, get in the habit of giving your teeth a good bedtime flossing and add years to your life.</p>
+                                            <h2>6. Get your sleep</h2>
+                                            <p>Sleep is crucial to our wellbeing. As we sleep, the brain clears away the debris of the day’s work
+                                                 while resetting and restoring nerve networks so that they can function fully when we wake.</p>
+                                            <p >We all know the most common effects caused by the lack of sleep—drowsiness, fatigue, lack of focus, and forgetfulness. But the consequences of sleep deprivation may go far beyond the well-known, 
+                                                and have possibly long-lasting effects on your brain. One recent study from Italy suggests that the consistent lack of
+                                                 sleep may cause the brain to start destroying itself.</p>
+                                            <p >Stated simply, the Italian researchers worked with mice, some getting as much sleep as they wanted while others were subjected to extreme sleep deprivation. The researchers then studied the activity
+                                                 of the glial cells that act as the brain’s caretakers, sweeping out unneeded brain cell connectors (a kind of brain junk) to keep the brain functioning normally. They found that the glial cells were far more active in the sleep deprived mice, 
+                                                 and it’s possible that this hyper-sweeping/destructive activity may contribute to Alzheimer’s and other brain disorders.</p>
+                                            <p>To avoid this potential threat, develop the habit of getting a solid 7-9 hours of sleep. If you’re having trouble dozing off, keep your bedtime routine free of TV, laptop, cell phone, and other devices, and give your brain some genuine downtime.</p>
+                                            
+                                            <h2>7. Challenge yourself</h2>
+                                
+                                            <p class="p1">We all get into ruts, doing the same things day after day, but to keep both body and mind agile, get into the habit of taking on challenges. And don’t feel
+                                             embarrassed about not being an expert. Remember that every expert was once also a beginner.</p>
+                                             <p class="p1">Take some art lessons and find your inner van Gogh.</p>
+                                             <p class="p1">How about learning another language? Your local library probably has language programs available at no cost to members. 
+                                             And there are plenty of free online language apps like Duolingo to help you.</p>
+                                             <p class="p1">Never had a chance to play a musical instrument? Get a harmonica for less than $30, along with some instructional CDs.
+                                              Practicing 30 minutes or so a day (great relaxation therapy), you’ll soon amaze your friends with the beautiful songs you can play.</p>
+
+                        </p>
+                    </div>
+                </article>
+                </div>
+                <Foot></Foot>
+            </div>
+             
+            
+                                              
+        )
+
+
+    }
+}
